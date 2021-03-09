d0e5105d3ae2853b10b28509a4e9130730547c44
Matthew <ashlander@live.com>
2021-03-09 01:21:24 -0600

    Nav Button Active Differentiation

--- static/js/controls.js
+++ static/js/controls.js
@@ -3,15 +3,34 @@ var tabs = {
 	vis: d3.select('#visbutt'),
 	data: d3.select('#databutt'),
 };
-
 var views = {
 	map: d3.select('#mapview'),
 	vis: d3.select('#visview'),
 	data: d3.select('#dataview'),
 };
 
-function tabChange(tab) {
-	console.log(tab)
-	d3.selectAll('.viewer').style('display', 'none');
-	d3.select(`#${tab}`).style('display', 'block');
+// Tab Change
+function tabChange(view) {
+	tabs[view].on('click', function () {
+		var allTabs = d3.selectAll('.navbutt');
+		var viewers = d3.selectAll('.viewer');
+		var activeTab = tabs[view];
+		var activeView = views[view];
+		
+		// disable everything
+		viewers.style('display', 'none');
+		allTabs.style('background-color', 'rgb(222, 184, 135)');
+
+		// re-enable appropriate controls
+		activeTab.style('background-color', 'rgba(222, 184, 135, .5)');
+		activeView.style('display', 'block');
+})};
+
+
+function init() {
+	tabChange('map');
+	tabChange('vis');
+	tabChange('data');
 };
+
+init();
